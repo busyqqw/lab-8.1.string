2736f332fb096553ddec1ada3beac1883f12a2bd
busyqqw <matsyta.g@gmail.com>
2023-11-16 18:31:56 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 8.1.string/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char row[] = "nlo";
+			int k = 1;
+			int t = Count(row);
+			Assert::AreEqual(k, t);
+		}
+	};
+}
